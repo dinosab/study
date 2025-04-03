af17c59a5f3d176e0065ccec3f21106df2812cab
sabMonster <sabina.deliu@gmail.com>
2025-04-03 02:38:16 -0700

    Initial import

---
+++ src/robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.boz'
+    }, 
+    {
+        id: 2,
+        name: 'Jackson Hitch',
+        username: 'Bret',
+        email: 'gonzalesd@april.boz'
+    },   {
+        id: 3,
+        name: 'Didums McGriff',
+        username: 'Bret',
+        email: 'heithere@april.boz'
+    },   {
+        id: 4,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.boz'
+    },   {
+        id: 5,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.boz'
+    },   {
+        id: 6,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.boz'
+    },   {
+        id: 7,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.boz'
+    },   {
+        id: 8,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.boz'
+    },
+    {
+        id: 9,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.boz'
+    }
+]
+
+export default robots;
